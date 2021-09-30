ad817a44ceffde04c5ed8132985e18d08c0aaf92
Hunter Haufler <hhaufler@ryanrts.com>
2021-09-30 14:53:07 -0500

    Small change to test expo publish on main

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{backgroundColor: "red"}}>Here's  pretty cool update! Don't you think!!</Text>
       <StatusBar style="auto" />
     </View>
   );
