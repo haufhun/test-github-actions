64195d9540ef74531a93f379a3ecc2b1d7316b70
Hunter Haufler <hhaufler@ryanrts.com>
2021-10-06 08:33:46 -0500

    An update to test

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text style={{backgroundColor: "red"}}>Here's  pretty cool update! Don't you think!!</Text>
+      <Text style={{ backgroundColor: "red" }}>
+        Here's pretty cool update! Don't you think!!
+      </Text>
+      <Text>Some update</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
